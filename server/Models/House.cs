79833a44e97ba4f53b9d861ead4873ce85217a47
ThomasWSnider <thomas.wayne.snider@gmail.com>
2024-08-13 14:33:45 -0600

    House Model is built

--- server/Models/House.cs
+++ server/Models/House.cs
@@ -2,5 +2,13 @@ namespace gregslist_dotnet.Models;
 
 public class House
 {
-
+  public int Id { get; set; }
+  public int Bedrooms { get; set; }
+  public int Bathrooms { get; set; }
+  public int Levels { get; set; }
+  public string ImgUrl { get; set; }
+  public int Year { get; set; }
+  public int Price { get; set; }
+  public string Description { get; set; }
+  public string CreatorId { get; set; }
 }
