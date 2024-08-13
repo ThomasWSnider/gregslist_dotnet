ef67ddba5cbf11fe3f4f8357dd72ee17cf5d981f
ThomasWSnider <thomas.wayne.snider@gmail.com>
2024-08-13 15:38:00 -0600

    getting houses- I mean homes

---
+++ server/Controllers/HousesController.cs
@@ -0,0 +1,27 @@
+namespace gregslist_dotnet.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class HousesController : ControllerBase
+{
+  private readonly HousesService _housesService;
+
+  public HousesController(HousesService housesService)
+  {
+    _housesService = housesService;
+  }
+
+  [HttpGet]
+  public ActionResult<List<House>> GetAllHouses()
+  {
+    try
+    {
+      List<House> houses = _housesService.GetAllHouses();
+      return Ok(houses);
+    }
+    catch (Exception exception)
+    {
+      return BadRequest(exception);
+    }
+  }
+}
--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -19,3 +19,8 @@ CREATE TABLE houses(
   creatorId VARCHAR(255) NOT NULL,
   FOREIGN KEY (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
 );
+
+INSERT INTO 
+houses (bedrooms, bathrooms, levels, imgUrl, year, price, description, creatorId)
+VALUES
+(1, 1, 1, "https://images.unsplash.com/photo-1502592238809-4cba3c1b80bc?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 1855, 1400000, "A reasonably priced house", "66abb758200ee1bc80c6579e")
